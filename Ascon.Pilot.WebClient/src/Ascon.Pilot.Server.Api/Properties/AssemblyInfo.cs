6a1acd20bafd124433a05f5decf672d89a280921
3nGercog <sereqa0813@rambler.ru>
2017-05-17 18:25:27 +0300

    Add  Ascon.Pilot.Server.Api Class Library

---
+++ Ascon.Pilot.WebClient/src/Ascon.Pilot.Server.Api/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ascon.Pilot.Server.Api")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f453c3c8-258f-47d6-8c80-3d219c708a1f")]
