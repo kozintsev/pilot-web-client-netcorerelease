d1e796e3b3f1bd11af2e43b3526e87cd2a8b0395
3nGercog <sereqa0813@rambler.ru>
2017-05-17 17:57:40 +0300

    add  Ascon.Pilot.Transport Class Library

---
+++ Ascon.Pilot.WebClient/src/Ascon.Pilot.Transport/MarshallingFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Ascon.Pilot.Transport
+{
+    public interface IMarshallingFactory
+    {
+        ICallService GetUnmarshaller(IImplementationFactory implementationFactory);
+        IGetService GetMarshaller(ICallService callService);
+    }
+
+    public class MarshallingFactory : IMarshallingFactory
+    {
+        private IGetService _marshaller;
+        private ICallService _unmarshaller;
+
+        public ICallService GetUnmarshaller(IImplementationFactory implementationFactory)
+        {
+            return _unmarshaller ?? (_unmarshaller = new Unmarshaller(implementationFactory));
+        }
+
+        public IGetService GetMarshaller(ICallService callService)
+        {
+            return _marshaller ?? (_marshaller = new Marshaller(callService));
+        }
+    }
+
+    public class JsonMarshallingFactory : IMarshallingFactory
+    {
+        private IGetService _marshaller;
+        private ICallService _unmarshaller;
+
+        public ICallService GetUnmarshaller(IImplementationFactory implementationFactory)
+        {
+            return _unmarshaller ?? (_unmarshaller = new JsonUnmarshaller(implementationFactory));
+        }
+
+        public IGetService GetMarshaller(ICallService callService)
+        {
+            return _marshaller ?? (_marshaller = new JsonMarshaller(callService));
+        }
+    }
+}
