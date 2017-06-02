c72dbe3d28df5d1e882459496ac2190c21fb1b6d
3nGercog <sereqa0813@rambler.ru>
2017-06-02 10:38:00 +0300

    del coment

--- Ascon.Pilot.WebClient/src/Ascon.Pilot.WebClient/ViewComponents/BredcrumbsViewComponent.cs
+++ Ascon.Pilot.WebClient/src/Ascon.Pilot.WebClient/ViewComponents/BredcrumbsViewComponent.cs
@@ -9,11 +9,7 @@ namespace Ascon.Pilot.WebClient.ViewComponents
 {
     public class BreadcrumbsViewComponent : ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync(Guid? id)
-        {
-            return await Task.Run(() => Breadcrumbs(id));
-        }
-        public IViewComponentResult Breadcrumbs(Guid? id)
+        public IViewComponentResult Invoke(Guid? id)
         {
             id = id ?? DObject.RootId;
             var types = HttpContext.Session.GetMetatypes();
@@ -22,7 +18,7 @@ namespace Ascon.Pilot.WebClient.ViewComponents
             Queue<KeyValuePair<string, string>> result = new Queue<KeyValuePair<string, string>>();
             Guid parentId = id.Value;
             bool isSource = ViewBag.IsSource ?? false;
-            while (parentId != DObject.RootId)
+            if(parentId != DObject.RootId)
             {
                 var obj = serverApi.GetObjects(new[] { parentId })[0];
                 var mType = types[obj.TypeId];
@@ -31,29 +27,9 @@ namespace Ascon.Pilot.WebClient.ViewComponents
                     isSource = false;
                 parentId = obj.ParentId;
             }
-            result.Enqueue(new KeyValuePair<string, string>(Url.Action("Index", "Files", new { id = DObject.RootId }), "Начало"));
+            else
+                result.Enqueue(new KeyValuePair<string, string>(Url.Action("Index", "Files", new { id = DObject.RootId }), "Начало"));
             return View(result);
         }
-        //public IViewComponentResult Invoke(Guid? id)
-        //{
-        //    id = id ?? DObject.RootId;
-        //    var types = HttpContext.Session.GetMetatypes();
-
-        //    var serverApi = HttpContext.GetServerApi();
-        //    Queue<KeyValuePair<string, string>> result = new Queue<KeyValuePair<string, string>>();
-        //    Guid parentId = id.Value;
-        //    bool isSource = ViewBag.IsSource ?? false;
-        //    while (parentId != DObject.RootId)
-        //    {
-        //        var obj = serverApi.GetObjects(new[] { parentId })[0];
-        //        var mType = types[obj.TypeId];
-        //        result.Enqueue(new KeyValuePair<string, string>(Url.Action("Index", "Files", new { id = obj.Id, isSource }), obj.GetTitle(mType)));
-        //        if (mType.IsMountable)
-        //            isSource = false;
-        //        parentId = obj.ParentId;
-        //    }
-        //    result.Enqueue(new KeyValuePair<string, string>(Url.Action("Index", "Files", new { id = DObject.RootId }), "Начало"));
-        //    return View(result);
-        //}
     }
 }
