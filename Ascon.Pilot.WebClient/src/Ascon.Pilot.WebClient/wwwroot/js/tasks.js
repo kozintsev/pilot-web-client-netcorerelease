72e4708fbf1d419404ceba716fdbdcf0d90b869c
InsecureBeast <isr3@mail.ru>
2017-10-11 23:08:33 +0300

    loading tasks by scrolling

--- Ascon.Pilot.WebClient/src/Ascon.Pilot.WebClient/wwwroot/js/tasks.js
+++ Ascon.Pilot.WebClient/src/Ascon.Pilot.WebClient/wwwroot/js/tasks.js
@@ -24,4 +24,43 @@
 
 $(document).ready(function () {
     loadTasks(6);
+
+    var win = $(window);
+    var doc = $(document);
+
+    // Each time the user scrolls
+    win.scroll(function () {
+        // Vertical end reached?
+        var height = doc.height() - win.height();
+        var pos = win.scrollTop();
+               
+        if (pos >= height - 50 && pos <= height) {
+            addTasks();
+        }
+    });
 });
+
+var ready = true; //Assign the flag here
+function addTasks() {
+    if (!ready)
+        return;
+
+    ready = false; //Set the flag here
+    $.ajax({
+        url: '/Tasks/GetNextTasks',
+        datatype: "json",
+        type: "post",
+        contenttype: 'application/json; charset=utf-8',
+        async: true,
+        success: function (data) {
+            $("#taskList").append(data);
+        },
+        error: function (xhr) {
+            alert('error' + xhr);
+        },
+        complete: function () {
+            $("#progress").hide();
+            ready = true; //Reset the flag here
+        },
+    });
+}
