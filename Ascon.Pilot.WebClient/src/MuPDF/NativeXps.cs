ec1cf137f1cea781d1ff7963256d590b199b1731
Demerit <igor.zubov.a@gmail.com>
2017-11-10 12:52:26 +0300

    Пофиксил рендеринг
    Пофиксил рендеринг

---
+++ Ascon.Pilot.WebClient/src/MuPDF/NativeXps.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MuPDF
+{
+    internal class NativeXps : IDisposable
+    {
+        private GCHandle _dataBufferHandle;
+        public IntPtr[] Pages { get; private set; }
+        public IntPtr XpsPtr { get; private set; }
+
+        public NativeXps(IntPtr docNativePtr, int pageCount, GCHandle dataHandle)
+        {
+            _dataBufferHandle = dataHandle;
+            XpsPtr = docNativePtr;
+            Pages = new IntPtr[pageCount];
+        }
+
+        public void Dispose()
+        {
+            _dataBufferHandle.Free();
+
+            if (XpsPtr == IntPtr.Zero)
+                return;
+
+            foreach (var page in Pages)
+            {
+                try
+                {
+                    RenderLibrary.close_page(XpsPtr, page);
+                }
+                catch
+                {
+                }
+            }
+            Pages = null;
+
+            try
+            {
+                RenderLibrary.close_xps(XpsPtr);
+            }
+            finally
+            {
+                XpsPtr = IntPtr.Zero;
+            }
+        }
+    }
+}
