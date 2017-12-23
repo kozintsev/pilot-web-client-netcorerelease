019182f69f914b87531451127bc65b904ceda2de
Oleg Kozintsev <o.kozintsev@gmail.com>
2017-12-23 16:46:34 +0400

    add Object and Task Modifier

---
+++ Ascon.Pilot.WebClient/src/Ascon.Pilot.WebClient/Server/ServerConnector.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using Ascon.Pilot.Server.Api;
+using Ascon.Pilot.Server.Api.Contracts;
+using Ascon.Pilot.WebClient.Models;
+
+namespace Ascon.Pilot.WebClient.Server
+{
+    public interface IServerConnector
+    {
+        IServerApi ServerApi { get; }
+        IFileArchiveApi FileArchiveApi { get; }
+        void Connect();
+        void Disconnect();
+        int PersonId { get; }
+    }
+
+    public class ServerConnector : IServerConnector
+    {
+        private readonly HttpPilotClient _client;
+        private ServerCallback _serverCallback;
+
+        public int PersonId { get; private set; }
+        public IServerApi ServerApi { get; private set; }
+        public IFileArchiveApi FileArchiveApi { get; private set; }
+
+        public ServerConnector()
+        {
+            _client = new HttpPilotClient();
+        }
+
+        public void Connect()
+        {
+            _client.Connect("");
+
+            _serverCallback = new ServerCallback();
+
+            ServerApi = _client.GetServerApi(_serverCallback);
+            ServerApi.OpenDatabase("11", "11", "11", false);
+
+            var people = ServerApi.LoadPeople();
+            var person = people.FirstOrDefault(p => !p.IsDeleted && p.Login == "");
+            if (person != null)
+                PersonId = person.Id;
+
+            FileArchiveApi = _client.GetFileArchiveApi("");
+        }
+
+        public void Disconnect()
+        {
+            _client?.Disconnect();
+        }
+    }
+}
+}
---
+++ Ascon.Pilot.WebClient/src/Ascon.Pilot.WebClient/Server/TaskModifier.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ascon.Pilot.Core;
+
+namespace Ascon.Pilot.WebClient.Server
+{
+    public interface ITaskModifier
+    {
+        void Apply(DChangesetData changesetData);
+        DChange EditStage(DObject stage);
+        DChange EditTask(DObject task);
+        DChange EditTaskById(Guid taskId);
+        DChange EditWorkflow(DObject workflow);
+        DObject NewAgreementTask(DObject workflow, DObject stage, int executorPosition);
+        DObject NewStage(DObject workflow);
+        IEnumerable<DObject> NewTask(string title, string description, IEnumerable<int> executors, DateTime deadLine);
+        IEnumerable<DObject> NewTask(string title, string description, IEnumerable<int> executors, DateTime deadLine, IEnumerable<Guid> attachments);
+        DObject NewTask(DObject workflow, DObject stage, int executorPosition);
+        DObject NewWorkflow();
+    }
+
+    public class TaskModifier : ITaskModifier
+    {
+        private readonly IServerConnector _connector;
+        private readonly int _personId;
+
+        public TaskModifier(IServerConnector connector, int personId)
+        {
+            _connector = connector;
+            _personId = personId;
+        }
+
+        public void Apply(DChangesetData changesetData)
+        {
+            _connector.ServerApi.Change(changesetData);
+        }
+
+        public DChange EditStage(DObject stage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DChange EditTask(DObject task)
+        {
+            var changed = task.Clone();
+            var change = new DChange { Old = task, New = changed };
+            return change;
+        }
+
+        public DChange EditTaskById(Guid taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DChange EditWorkflow(DObject workflow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DObject NewAgreementTask(DObject workflow, DObject stage, int executorPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DObject NewStage(DObject workflow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DObject> NewTask(string title, string description, IEnumerable<int> executors, DateTime deadLine)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DObject> NewTask(string title, string description, IEnumerable<int> executors, DateTime deadLine,
+            IEnumerable<Guid> attachments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DObject NewTask(DObject workflow, DObject stage, int executorPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DObject NewWorkflow()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
